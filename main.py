f45295943a4d55fd5786fba262b2f74b5c246065
Genius Jamir <75374424+Genius98@users.noreply.github.com>
2021-03-31 10:30:49 +0600

    Using Turtle Graphics

---
+++ main.py
@@ -0,0 +1,23 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+
+def draw_spirograph(size_of_gap): #between each circle the size of gap
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap) #changing the center heading lil bit shifted so we add the size of gap & current center heading
+
+draw_spirograph(10) #10 is the size of each gap
+
+
